9359305335fe0877e26ef88a5632f4573ea138aa
braisv <brais.vidal.rodriguez@gmail.com>
2019-11-12 12:53:10 +0100

    step 8

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import css from './styles/style.styl';
+
 import Main from './Components/Main.js'
 import Single from './Components/Single.js'
 import PhotoGrid from './Components/PhotoGrid.js'
 
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
 const router = (
-    <Router history={browserHistory}>
+    <Provider store={store}>
+    <Router history={history}>
         <Route path="/" component={Main}>
             <IndexRoute component={PhotoGrid}></IndexRoute>
             <Route path="/view/:postId" component={Single}></Route>
         </Route>
     </Router>
+    </Provider>
+
 )
 
 render(router, document.getElementById('root'));
